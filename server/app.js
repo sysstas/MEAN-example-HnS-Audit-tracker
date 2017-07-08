59868133e3d518d789dacde89dcb5f62bfbb0e11
Stanislav Sysoiev <sysstas@gmail.com>
2017-07-08 16:33:00 +0300

    add angularJs as frontend framework

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@
 let express = require('express');
 let app = express();
 
-/*app.use( express.static(__dirname + "/../client") );*/
+app.use( '/', express.static(__dirname + "/../client") );
+app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 
 
 let serverPort = 3000;
