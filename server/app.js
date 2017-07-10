65f6177718d89ea36366632fd3605683f1fe6169
Stanislav Sysoiev <sysstas@gmail.com>
2017-07-10 14:13:37 +0300

    change hardcoded express dump to call to MondoDB

--- server/app.js
+++ server/app.js
@@ -2,11 +2,27 @@
 
 let express = require('express');
 let app = express();
+let MongoClient = require('mongodb').MongoClient;
+
+let arr = [];
+MongoClient.connect('mongodb://localhost:27017/auditors', (error, db) => {
+	console.log('Connected to Mongo');
+
+	
+	let querry = {};
+	let cursor = db.collection('auditors').find(querry);
+	
+	cursor.forEach(function(doc){
+		arr.push(doc);
+	});
+
+	
+});
 
 app.use( '/', express.static(__dirname + "/../client") );
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 
-var arr = [
+/*var arr = [
 		{
 			name: "Iron Man",
 			audits: [{month: "june", amount: 36}]
@@ -19,9 +35,10 @@ var arr = [
 			name: "Spider Men",
 			audits: [{month: "june", amount: 17}]
 		}
-	];
+	];*/
 
 app.get('/auditors', (req,res)=>{
+	console.log(arr);
 	res.json(arr);
 });
 
